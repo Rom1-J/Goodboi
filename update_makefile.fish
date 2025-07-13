1ebe0407516090676f0db7f48048d5166c06a2b3
Romain J. <romain@gnous.eu>
2025-07-13 17:51:12 +0200

    update(build): migrate .fish to ./configure

--- update_makefile.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-set -l obj_files (find src -type f -name "*.c" | sed 's/\.c$/.o/')
-set -l rk_version (echo (date +%s):(git rev-parse --short HEAD 2>/dev/null || echo "N/A"))
-
-sed "s|{{OBJS_FILES}}|$obj_files|g" Makefile.template \
-    | sed "s|{{RK_VERSION}}|$rk_version|g" > Makefile
